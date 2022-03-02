a027bb9eac324448bcbe465a4a23f5a2939ab435
polar <polaris402@naver.com>
2022-03-02 10:46:33 +0900

    mysite4 - messageConveter 적용 0302

--- mysite04/src/main/java/com/poscoict/config/web/MvcConfig.java
+++ mysite04/src/main/java/com/poscoict/config/web/MvcConfig.java
@@ -1,7 +1,17 @@
 package com.poscoict.config.web;
 
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -28,8 +38,51 @@ public class MvcConfig extends WebMvcConfigurerAdapter {  //WebMvcConfigurerAdap
 		}
 		
 		// Message Converter
+		@Bean
+		public StringHttpMessageConverter stringHttpMessageConverter() {
+			StringHttpMessageConverter messageConverter = new StringHttpMessageConverter();
+			
+			
+			
+//			List<MediaType> list = new ArrayList<>();
+//			list.add(new MediaType("text","html", Charset.forName("utf-8")));
+			
+//			Arrays.asList(new MediaType("text","html", Charset.forName("utf-8")));
+			messageConverter.setSupportedMediaTypes(
+					Arrays.asList(
+							new MediaType("text","html", Charset.forName("utf-8"))
+					)
+				);
+			
+			return messageConverter;
+		}
 		
-//		//DefaultServlet 위임(delegate) Handler
+		
+		@Bean
+		public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+			Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder()
+					.indentOutput(true)
+					.dateFormat(new SimpleDateFormat("yyyy-mm-dd"));
+			
+			MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter(builder.build());
+			messageConverter.setSupportedMediaTypes(
+					Arrays.asList(
+							new MediaType("application", "json", Charset.forName("utf-8"))
+					)
+			);
+			return messageConverter;		
+		}
+		
+		
+		
+		
+		@Override
+		public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+			converters.add(stringHttpMessageConverter());
+			converters.add(mappingJackson2HttpMessageConverter());
+		}
+
+		//		//DefaultServlet 위임(delegate) Handler
 		@Override
 		public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 			configurer.enable();
